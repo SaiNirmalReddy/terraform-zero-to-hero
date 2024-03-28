3c1c8db70fad01ff0ff3b71a5e7b0a0b6e17d382
SaiNirmalReddy <126653988+SaiNirmalReddy@users.noreply.github.com>
2024-03-29 02:46:05 +0530

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -6,6 +6,4 @@ resource "aws_instance" "example" {
     ami           = "ami-0914547665e6a707c"  # Specify an appropriate AMI ID
     instance_type = "t3.large"
     subnet_id     = "subnet-073f2649fd10abd91"
-    key_name      = "terraform"
-    
 }
