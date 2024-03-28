4da396b8c8fc43bd31f9884bfdc543a736f5531b
SaiNirmalReddy <126653988+SaiNirmalReddy@users.noreply.github.com>
2024-03-29 01:16:22 +0530

    Update main.tf
    Updated subnet

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -5,5 +5,5 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami           = "ami-0914547665e6a707c"  # Specify an appropriate AMI ID
     instance_type = "t3.large"
-    subnet_id = "subnet-0c59ef1864cbbbe3e"
+    subnet_id = "subnet-088649ed10f5f49c8"
 }
