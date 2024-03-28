64f5a7da4ef877e9e7da7f4386f0cf8b5f820304
SaiNirmalReddy <126653988+SaiNirmalReddy@users.noreply.github.com>
2024-03-29 01:23:22 +0530

    Update main.tf
    updated subnet by creating new vpc other than default vpc

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -5,5 +5,5 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami           = "ami-0914547665e6a707c"  # Specify an appropriate AMI ID
     instance_type = "t3.large"
-    subnet_id = "subnet-088649ed10f5f49c8"
+    subnet_id = "subnet-073f2649fd10abd91"
 }
